43e2c5214a031af3317c7722ee1ce71916bb235a
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-02-21 10:16:39 +0530

    add base and start workoin on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import Image from "next/image";
-
+import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 export default function Home() {
   return (
-    <div>
-      <h1>Hippo</h1>
-      <main>
-        
-      </main>
-    </div>
+    <MaxWidthWrapper>
+      <div className="flex flex-col items-center text-center max-w-3xl mx-auto py-20">
+        <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality {" "}
+          <span className="text-blue-600">
+            Digital Assets
+          </span>
+          .
+        </h1>
+        <p className="text-lg max-w-proset text-muted-foreground mt-6">
+            Welcome  to DigitalHippo. Every assets on your platform is
+            verified by our team to ensurre higher qulity standards.
+        </p>
+      </div>
+    </MaxWidthWrapper>
   );
 }
