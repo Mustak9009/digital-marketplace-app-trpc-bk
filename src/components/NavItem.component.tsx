34b3740b0bbc2cc8b518f623e07c768ea106c0dd
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-02-24 12:03:42 +0530

    add navItem and img

--- src/components/NavItem.component.tsx
+++ src/components/NavItem.component.tsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import { Button } from './ui/button'
-
-export default function NavItem() {
+import React from "react";
+import { Button } from "./ui/button";
+import { PRODUCT_CATEGORIES } from "@/config/index.config";
+import { ChevronDown } from 'lucide-react';
+import { cn } from "@/lib/utils";
+import Image from 'next/image'
+import Link from 'next/link'
+interface NavItemPropsType {
+  category: (typeof PRODUCT_CATEGORIES)[number];
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
+export default function NavItem({category,handleOpen,isOpen,isAnyOpen}: NavItemPropsType) {
   return (
-    <div className='flex'>
-        <div className='flex items-center relative'>
-            <Button>a</Button>
+    <div className="flex">
+      <div className="flex items-center relative">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
+          <ChevronDown className={cn("size-4 transition-transform text-muted-foreground",{
+            'rotate-180':isOpen
+          })}/>
+        </Button>
+      </div>
+      {isOpen ? (
+        <div className={cn("absolute inset-x-0 top-full text-sm text-muted-foreground",{
+          "animate-in fade-in-10 slide-in-from-top-5":!isOpen //The tailwind class comes from -> tailwindcss-animate package
+        })}>
+          <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden={true}/>
+          <div className="relative bg-white">
+            <div className="max-w-7xl mx-auto px-8">
+              <div className="grid grid-cols-4 gap-x-4 gap-y-8 py-16">
+                <div className="grid grid-cols-3 gap-x-8 col-span-4 col-start-1">
+                  {category.featured.map((item)=>(
+                    <div key={item.name} className="relative text-base sm:text-sm group">
+                      <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                        <Image src={item.imageSrc} alt={item.name} className="object-cover object-center" fill/>
+                      </div>
+                      <Link href={item.href} className="font-medium text-gray-900 block mt-6">
+                        {item.name}
+                      </Link>
+                      <p aria-hidden={true}>Shop now</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      ) : null}
     </div>
-  )
+  );
 }
--- src/components/NavItems.component.tsx
+++ src/components/NavItems.component.tsx
@@ -5,16 +5,21 @@ import {PRODUCT_CATEGORIES} from '@/config/index.config';
 import NavItem from '@/components/NavItem.component';
 export default function NavItems() {
   const [activeIndex,setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
   return (
     <div className='flex gap-4 h-full'>
-      {PRODUCT_CATEGORIES.map((categori,index)=>{
+      {PRODUCT_CATEGORIES.map((category,index)=>{
         const handleOpen = ()=>{
-          if(activeIndex===index) setActiveIndex(null);
-          setActiveIndex(index)
+          if(activeIndex===index){ //Give double click functionality
+            setActiveIndex(null);
+          }else{
+            setActiveIndex(index)
+          }
         }
         const isOpen = index === activeIndex; 
         return(
-          <NavItem/>
+          <NavItem key={category.value} {...{category,handleOpen,isOpen,isAnyOpen}}/>
         )
       })}
     </div>
